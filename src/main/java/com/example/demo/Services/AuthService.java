b29af8057fea7e4abc012e183f737250f4389205
AndreiCalugar <andrei.nicolae.calugar@gmail.com>
2021-10-27 13:41:06 +0200

    Created basic auth method

--- src/main/java/com/example/demo/Services/AuthService.java
+++ src/main/java/com/example/demo/Services/AuthService.java
@@ -1,8 +1,19 @@
 package com.example.demo.Services;
 
 import com.example.demo.Models.App;
+import org.apache.tomcat.util.http.HeaderUtil;
+import org.springframework.http.RequestEntity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Random;
 
 public class AuthService {
@@ -11,9 +22,13 @@ public class AuthService {
     public String GetAuthenticationRequest(App app){
         var authUrl =app.getAuthorizationEndpoint();
         var state =GetRandomString(12);
-
+        var redirectUri = new URL(app.setRedirectUrls(););
     }
 
+
+    private HeaderUtil getBasicAuthHeader(){
+
+    }
 
     static String GetRandomString(int n)
     {
@@ -41,5 +56,21 @@ public class AuthService {
         }
 
         return sb.toString();
+    }
+
+    private void GetBasicAuthHeader(String clientId,String secret) throws IOException {
+        // add url name
+        URL url = new URL("http://ip:port/login");
+        String encoding = Base64.getEncoder().encodeToString(("test1:test1").getBytes(StandardCharsets.UTF_8));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Authorization", "Basic " + encoding);
+        InputStream content = (InputStream) connection.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(content));
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
